3116b798291b679989068ae0f10ac124feecf48d
pardahlman <par.dahlman@fellowdeveloper.se>
2017-09-08 21:14:44 +0200

    Upgrade to 2.0 of packages§

---
+++ RawRabbit.Foo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RawRabbit.Foo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
